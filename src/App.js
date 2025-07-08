338d92c6383e803e562e6a6abbfa5fc4ade99acc
vidit2005368 <t8806904@gmail.com>
2025-07-08 19:24:21 +0530

    first react app

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          vidit was here  
         </a>
       </header>
     </div>
